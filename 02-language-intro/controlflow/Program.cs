51e79a3413556130cd36d73760e1c54f7ce82360
ztarbug <kronprinzfriedrich@gmx.de>
2024-08-18 17:13:52 +0200

    work on control flow

--- 02-language-intro/controlflow/Program.cs
+++ 02-language-intro/controlflow/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/**
+ * This example shows various options to control the flow of a program in C#.
+ */
+class ControlFlow
+{
+    static void Main()
+    {
+        int i = 5;
+        if (i > 10)
+        {
+            Console.WriteLine("Number was larger");
+        }
+        else
+        {
+            Console.WriteLine("Number was smaller");
+        }
+
+        string? input; //? for C# 8 - careful null checking
+        Console.WriteLine("Please enter your age: ");
+        input = Console.ReadLine();
+        int a = Convert.ToInt32(input);
+        Console.WriteLine("Your age is: {0}", a);
+
+        string? name;
+        Console.WriteLine("Please enter your name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Your name is: {0}", name);        
+    }
+}
+
