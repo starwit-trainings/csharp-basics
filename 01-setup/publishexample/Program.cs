db756fd3b45edc11f5e9279c5eef142c81332a44
ztarbug <kronprinzfriedrich@gmx.de>
2024-08-18 18:11:33 +0200

    work on examples

--- 01-setup/publishexample/Program.cs
+++ 01-setup/publishexample/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+
+Console.WriteLine("This application is running as a standalone package. Now let's try to find out, which operating system, we are running on.");
+var windows = OSPlatform.Windows;
+var linux = OSPlatform.Linux;
+var macos = OSPlatform.OSX;
+var bsd = OSPlatform.FreeBSD;
+
+if(RuntimeInformation.IsOSPlatform(windows)) 
+{
+    Console.WriteLine("I am running on Windows");
+}
+
+if(RuntimeInformation.IsOSPlatform(linux)) 
+{
+    Console.WriteLine("I am running on Linux");
+}
+
+if(RuntimeInformation.IsOSPlatform(macos)) 
+{
+    Console.WriteLine("I am running on MacOS");
+}
+
+if(RuntimeInformation.IsOSPlatform(bsd)) 
+{
+    Console.WriteLine("I am running on Free BSD");
+}
